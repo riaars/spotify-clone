fa5b99c1bae51dc468cfde5f58bb427e946da521
Ria <riaratnasari@outlook.com>
2022-10-23 17:37:46 +0200

    add spotify web api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css'
 import Login from './Login'
 import Player from './Player'
 import { getTokenFromUrl } from './spotify'
+import SpotifyWebApi from 'spotify-web-api-js'
+
+const spotify = new SpotifyWebApi()
 
 function App() {
   const [token, setToken] = useState(null)
@@ -11,6 +14,7 @@ function App() {
     const _token = getTokenFromUrl()
     if (_token) {
       setToken(_token.access_token)
+      spotify.setAccessToken(_token)
     }
   }, [])
 
