11e5fc6ff5d3708628f375f817962512bde6fe2a
Ria <riaratnasari@outlook.com>
2022-10-23 21:02:24 +0200

    add banner

--- src/Body.js
+++ src/Body.js
@@ -6,6 +6,14 @@ const Body = ({ spotify }) => {
   return (
     <div className="body">
       <Header spotify={spotify} />
+      <div className="body_info">
+        <img src="" alt="" />
+        <div className="body_infoText">
+          <strong>PLAYLISTS</strong>
+          <h2>Discover Weekly</h2>
+          <p>Description ...</p>
+        </div>
+      </div>
     </div>
   )
 }
