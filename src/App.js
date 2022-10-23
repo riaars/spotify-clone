e673a5a3b72dbc5c76ec5c425dcaae2387261c43
Ria <riaratnasari@outlook.com>
2022-10-23 17:33:56 +0200

    handling login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Login from './Login'
+import Player from './Player'
+import { getTokenFromUrl } from './spotify'
 
 function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  )
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    const _token = getTokenFromUrl()
+    if (_token) {
+      setToken(_token.access_token)
+    }
+  }, [])
+
+  return <div className="app">{token ? <Player /> : <Login />}</div>
 }
 
 export default App
